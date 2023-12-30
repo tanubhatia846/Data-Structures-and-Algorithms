8b22918d02f210532ae8105c6159a6421fbb4121
tanubhatia846 <151834014+tanubhatia846@users.noreply.github.com>
2023-12-30 20:38:41 +0000

    sorting

--- 01_array/02_insertionsort.cpp
+++ 01_array/02_insertionsort.cpp
@@ -1,2 +1,31 @@
 // insert an element from unsorted array to its correct position in sorted array
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the size of the array:";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the elements in the array:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i = 1;i<n;i++)
+    {
+        int current = arr[i];
+        int j = i-1;
+          while(arr[j] > current && j>=0)
+          {
+            arr[j+1] = arr[j];
+            j--;
+          }
+        arr[j+1] = current;
+    }
+    for(int j=0;j<n;j++)
+    {
+        cout<<arr[j]<<" ";
+    }
+    return 0;
+}
